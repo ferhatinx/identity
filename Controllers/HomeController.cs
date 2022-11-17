e140d092d1d3c27b2c96088b444007a275dd6106
Ferhat Ersoy <mehmetinix@gmail.com>
2022-11-18 00:20:29 +0300

    btlcreate

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,53 @@
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 
 namespace Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<AppUser> _usermanager;
+
+        public HomeController(UserManager<AppUser> usermanager)
+        {
+            _usermanager = usermanager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new UserCreateModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(UserCreateModel model)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                AppUser user = new()
+                {
+                    Email=model.Email,
+                    UserName=model.UserName,
+                    Gender=model.Gender
+                };
+                var identityResult = await _usermanager.CreateAsync(user, model.Password);
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+                
+            }
+            return View(model);
         }
     }
 }
---
+++ Models/UserCreateModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class UserCreateModel
+    {
+        [Required(ErrorMessage ="Kullanıcı Adı Giriniz")]
+        public string UserName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Mail giriniz")]
+        [Required(ErrorMessage = "Email Giriniz")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Şifre Giriniz")]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage = "Şifre Doğru Giriniz")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage ="Cinsiyet Giriniz")]
+        public string Gender { get; set; }
+    }
+}
